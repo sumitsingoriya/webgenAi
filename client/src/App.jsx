5cc3c8a084ae1600c2a33ed6f476194d58ac37f5
sumitsingoriya <146583636+sumitsingoriya@users.noreply.github.com>
2026-07-19 15:23:47 +0530

    Change server URL to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import WebsiteEditor from "./pages/Editor";
 import LiveSite from "./pages/LiveSite";
 import Pricing from "./pages/Pricing";
 
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://webgenai-fnal.onrender.com";
 function App() {
   useGetCurrentUser();
 
