0ac57121ce94317938314bcfd62f457cd62bd4e8
sumitsingoriya <146583636+sumitsingoriya@users.noreply.github.com>
2026-07-19 15:59:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,6 +25,9 @@ app.use("/api/user",userRouter)
 app.use("/api/website",websiteRouter)
 app.use("/api/billing",billingRouter)
 
+app.get("/", (req, res) => {
+    res.send("Backend Running Successfully 🚀");
+});
 
 app.listen(port,()=>{
     console.log("server started")
